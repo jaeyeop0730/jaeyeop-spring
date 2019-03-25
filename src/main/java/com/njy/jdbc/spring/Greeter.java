04bd782ac05d8c3a2747e107ac1898d5f5d1759e
jaeyeop <kal205@naver.com>
2019-03-25 13:34:34 +0900

    first commit

---
+++ src/main/java/com/njy/jdbc/spring/Greeter.java
@@ -0,0 +1,12 @@
+package com.njy.jdbc.spring;
+
+public class Greeter {
+	public Greeter()
+	{
+		System.out.println("Greeter instance Created.");
+	}
+	public String greet()
+	{
+		return "Hello World!";
+	}
+}
